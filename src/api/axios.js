c6bf0a9d643b7a5d5cc8657d6d9ed370371f270b
Victor Ayoola <2468vik@gmail.com>
2025-10-28 01:09:31 +0100

    changed url to working backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const devURL = 'http://localhost:5000';
-const buildURL = 'https://fleet-diagram-476213-v8.uc.r.appspot.com/';
+const buildURL =
+  'https://budgetbuddy-backend-317312360411.us-central1.run.app/';
 const api = axios.create({
   baseURL: buildURL,
   withCredentials: true, // send cookies automatically
